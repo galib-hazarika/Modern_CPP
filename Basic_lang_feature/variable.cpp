270ee8c0434e81f932e5ff07854d0dde5a071302
galib hazarika <hazarika.galib@gmail.com>
2022-08-16 16:28:00 +0530

    Basic variable

---
+++ Basic_lang_feature/variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+	//scalar
+	int i = 9;
+	char c = 'a';
+	bool flag = 0;
+	bool flag1 = true;
+	float f = 8.9f;
+
+	double s = 8.900;
+
+	//vector
+	int a[7];
+	int a1[9] = {9, 8, 7, 6, 5, 3, 2, 1, 5};
+
+	std::cout << i << c << flag << flag1 << f << s;
+
+}
