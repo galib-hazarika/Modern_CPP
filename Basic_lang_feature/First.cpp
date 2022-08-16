65ea750bd28d74a4d74093667c1a94e48a055ed7
galib hazarika <hazarika.galib@gmail.com>
2022-08-16 11:02:25 +0530

    Fits program is added

---
+++ Basic_lang_feature/First.cpp
@@ -0,0 +1,22 @@
+//This is single line comment
+
+/* This is a multi line comment */
+
+// Standard header file donot end with .h extension
+
+/*
+namespace is  to wrap types so that they aren't visible outside
+std is a namespace
+to use a memeber of namespace first have to open the namespace
+*/
+
+/* Use full qualified name accessing memeber of namespace */
+
+#include <iostream>
+
+
+int main() {
+	//using namespace std;
+	std::cout << "Hello" << " C++" << std::endl;
+	return 0;
+}
